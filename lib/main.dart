55d5ed268635bf0b09d2bf8d75320896a4fbefda
onur <71764820+onue2@users.noreply.github.com>
2022-11-23 16:20:07 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -24,15 +24,19 @@ class GameArea extends StatefulWidget {
   State<GameArea> createState() => GameAreaState();
 }
 
+String xicon = " ";
+
 class GameAreaState extends State<GameArea> {
+  String yazi = " ";
+  List<String> yazilar = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Container(
-            height: 315,
-            width: 315,
-            color: Colors.black,
+            height: 320,
+            width: 320,
+            // color: Colors.blue,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
@@ -40,24 +44,54 @@ class GameAreaState extends State<GameArea> {
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     InkWell(
+                      onTap: () {
+                        setState(() {
+                          if (yazilar[0] == " ") {
+                            yazilar[0] = "x";
+                          } else if (yazilar[0] == "x") {
+                            yazilar[0] = " ";
+                          }
+                        });
+                      },
                       child: Container(
                         height: 95,
                         width: 95,
                         color: Colors.white,
+                        child: Text(yazilar[0]),
                       ),
                     ),
                     InkWell(
+                      onTap: () {
+                        setState(() {
+                          if (yazilar[1] == " ") {
+                            yazilar[1] = "x";
+                          } else if (yazilar[1] == "x") {
+                            yazilar[1] = " ";
+                          }
+                        });
+                      },
                       child: Container(
                         height: 95,
                         width: 95,
                         color: Colors.white,
+                        child: Text(yazilar[1]),
                       ),
                     ),
                     InkWell(
+                      onTap: () {
+                        setState(() {
+                          if (yazilar[2] == " ") {
+                            yazilar[2] = "x";
+                          } else if (yazilar[2] == "x") {
+                            yazilar[2] = " ";
+                          }
+                        });
+                      },
                       child: Container(
                         height: 95,
                         width: 95,
                         color: Colors.white,
+                        child: Text(yazilar[2]),
                       ),
                     ),
                   ],
@@ -66,24 +100,54 @@ class GameAreaState extends State<GameArea> {
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     InkWell(
+                      onTap: () {
+                        setState(() {
+                          if (yazilar[3] == " ") {
+                            yazilar[3] = "x";
+                          } else if (yazilar[3] == "x") {
+                            yazilar[3] = " ";
+                          }
+                        });
+                      },
                       child: Container(
                         height: 95,
                         width: 95,
                         color: Colors.white,
+                        child: Text(yazilar[3]),
                       ),
                     ),
                     InkWell(
+                      onTap: () {
+                        setState(() {
+                          if (yazilar[4] == " ") {
+                            yazilar[4] = "x";
+                          } else if (yazilar[4] == "x") {
+                            yazilar[4] = " ";
+                          }
+                        });
+                      },
                       child: Container(
                         height: 95,
                         width: 95,
                         color: Colors.white,
+                        child: Text(yazilar[4]),
                       ),
                     ),
                     InkWell(
+                      onTap: () {
+                        setState(() {
+                          if (yazilar[5] == " ") {
+                            yazilar[5] = "x";
+                          } else if (yazilar[5] == "x") {
+                            yazilar[5] = " ";
+                          }
+                        });
+                      },
                       child: Container(
                         height: 95,
                         width: 95,
                         color: Colors.white,
+                        child: Text(yazilar[5]),
                       ),
                     ),
                   ],
@@ -92,24 +156,54 @@ class GameAreaState extends State<GameArea> {
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     InkWell(
+                      onTap: () {
+                        setState(() {
+                          if (yazilar[6] == " ") {
+                            yazilar[6] = "x";
+                          } else if (yazilar[6] == "x") {
+                            yazilar[6] = " ";
+                          }
+                        });
+                      },
                       child: Container(
                         height: 95,
                         width: 95,
                         color: Colors.white,
+                        child: Text(yazilar[6]),
                       ),
                     ),
                     InkWell(
+                      onTap: () {
+                        setState(() {
+                          if (yazilar[7] == " ") {
+                            yazilar[7] = "x";
+                          } else if (yazilar[7] == "x") {
+                            yazilar[7] = " ";
+                          }
+                        });
+                      },
                       child: Container(
                         height: 95,
                         width: 95,
                         color: Colors.white,
+                        child: Text(yazilar[7]),
                       ),
                     ),
                     InkWell(
+                      onTap: () {
+                        setState(() {
+                          if (yazilar[8] == " ") {
+                            yazilar[8] = "x";
+                          } else if (yazilar[8] == "x") {
+                            yazilar[8] = " ";
+                          }
+                        });
+                      },
                       child: Container(
                         height: 95,
                         width: 95,
                         color: Colors.white,
+                        child: Text(yazilar[8]),
                       ),
                     ),
                   ],
